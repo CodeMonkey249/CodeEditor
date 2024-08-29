9df4c24046bfb81ab73739bb8bb30ce40e4c25b9
James Monk <james.monk2@protonmail.com>
2024-08-29 16:08:09 -0700

    error handling

--- src/kilo.c
+++ src/kilo.c
@@ -1,14 +1,24 @@
+/*** includes ***/
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
 struct termios orig_termios;
+
+/*** terminal ***/
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
 
 void disableRawMode(void) {
     /* Restore terminal flags */
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    int code = tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (code == -1) die("tcsetattr");
 }
 
 void enableRawMode(void) {
@@ -17,13 +27,15 @@ void enableRawMode(void) {
     Remove all preprocessing of input text.
     */
     // Store original terminal settings 
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    int code = tcgetattr(STDIN_FILENO, &orig_termios);
+    if (code == -1) die("tcgetattr");
     // Restore terminal at end of session
     atexit(disableRawMode);
     // We will store the raw settings here
     struct termios raw = orig_termios;
     // Get the current settings
-    tcgetattr(STDIN_FILENO, &raw);
+    code = tcgetattr(STDIN_FILENO, &raw);
+    if (code == -1) die("tcgetattr");
 
     ///////////
     // Flags //
@@ -41,12 +53,14 @@ void enableRawMode(void) {
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+/*** init ***/
 int main(void) {
     enableRawMode();
 
     while (1) {
         char c = '\0';
-	    read(STDIN_FILENO, &c, 1);
+	    int code = read(STDIN_FILENO, &c, 1);
+        if (code == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c)) {
             printf("%d\n", c);
         } else if (c != 'q') {
